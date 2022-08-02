f687c1d704a7a004116d36891d14593cdec2fb5d
yusufElb <108877646+yusufElb@users.noreply.github.com>
2022-08-02 13:46:54 +0100

    Time: 170 ms (76.64%), Space: 53.6 MB (50.59%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+    const cache = {};
+    
+    for(let str of strs){
+        
+        let sortedKey = str.split('').sort().join(''); //now aet in first example
+        
+        (cache[sortedKey]) ? cache[sortedKey].push(str) : cache[sortedKey] = [str];
+        //if the key exists - push the string to it : if it doesn't exist, create an array with that string
+    }
+  
+    return Object.values(cache);
+    //returns the arrays of all elements in the key
+};
